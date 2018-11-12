0d9a6e5c134431d20fee878bec230296c0e5d487
许林 <xulin.wh@qq.com>
2018-11-12 15:46:19 +0800

    第7章 通过IBOutlets更新View 完成

--- MyWeather/MyWeather/ViewController.swift
+++ MyWeather/MyWeather/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager:CLLocationManager = CLLocationManager()
     
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var temperature: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
@@ -23,12 +26,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         print(UIDevice.current.systemVersion)
         //if(ios8()) {
-            locationManager.requestAlwaysAuthorization()
+        locationManager.requestAlwaysAuthorization()
         //}
         
         locationManager.startUpdatingLocation()
     }
-
+    
     func ios8() -> Bool {
         return UIDevice.current.systemVersion > "8.0"
     }
@@ -56,25 +59,154 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                     parameters: params,
                     success: { (operation: AFHTTPRequestOperation,
                         responseObject: Any) in
-                        var info = responseObject as? NSDictionary
-                        print(info);
-                        print("OK")
+                        let info = responseObject as? NSDictionary
+                        // print(info);
+                        //  print("OK")
                         
-                       // self.updateUISuccess(responseObject as NSDictionary!)
+                        // self.updateUISuccess(responseObject as? NSDictionary!)
+                        self.updateUISuccess(jsonResult: info)
         },
                     failure: { (operation: AFHTTPRequestOperation!,
                         error: Error!) in
                         print("Error: " + error.localizedDescription)
                         
-                       // self.loading.text = "Internet appears down!"
+                        // self.loading.text = "Internet appears down!"
         })
         
+    }
+    
+    func updateUISuccess(jsonResult:NSDictionary!) {
+        // if let tempResult = (jsonResult["main"] as! NSDictionary)["temp"] as? Double {
+        if let tempResult = (jsonResult["main"] as! NSDictionary)["temp"] as? Double {
+            var temperature: Double = 0.0
+            if((jsonResult["sys"] as! NSDictionary)["country"] as? String == "US") {
+                temperature = round(((tempResult - 273.15) * 1.8) + 32)
+            }
+            else {
+                temperature = round(tempResult - 273.15)
+            }
+            
+            print(temperature)
+            self.temperature.text = "\(temperature)°"
+            self.temperature.font = UIFont.boldSystemFont(ofSize: 60)
+            //self
+            
+            let name = jsonResult["name"] as! String
+            self.location.font = UIFont.boldSystemFont(ofSize: 25)
+            print("name: ")
+            print(name)
+            self.location.text = "\(name)"
+            
+            var condition:Int = 1
+            condition = ((jsonResult["weather"] as! NSArray)[0] as! NSDictionary)["id"] as! Int
+            let sunrise = (jsonResult["sys"] as! NSDictionary)["sunrise"] as? Double
+            let sunset = (jsonResult["sys"] as! NSDictionary)["sunset"] as? Double
+            
+            var nightTime = false
+            let now = NSDate().timeIntervalSince1970
+            //精确到秒
+            let nowSecond = Double(round(now))
+            var sunriseDouble:Double = 0.0
+            if(sunrise != nil) {
+                sunriseDouble = sunrise!
+            }
+            
+            var sunsetDouble:Double = 0.0
+            if(sunset != nil) {
+                sunsetDouble = sunset!
+            }
+            
+            if(nowSecond < sunriseDouble || nowSecond > sunsetDouble) {
+                nightTime = true
+            }
+            
+            print(condition)
+            print(nightTime)
+            self.updateWeatherIcon(condition: condition, nightTime: nightTime)
+        } else {
+            
+        }
+    }
+    
+    func updateWeatherIcon(condition: Int, nightTime: Bool) {
+        if(condition < 300) {
+            if(nightTime) {
+                self.icon.image = UIImage(named: "tstorm1_night")
+            } else {
+                self.icon.image = UIImage(named: "tstorm1")
+            }
+        }
+            // Drizzle
+        else if (condition < 500) {
+            self.icon.image = UIImage(named: "light_rain")
+            
+        }
+            // Rain / Freezing rain / Shower rain
+        else if (condition < 600) {
+            
+            self.icon.image = UIImage(named: "shower3")
+        }
+            // Snow
+        else if (condition < 700) {
+            self.icon.image = UIImage(named: "snow4")
+        }
+            // Fog / Mist / Haze / etc.
+        else if (condition < 771) {
+            if nightTime {
+                self.icon.image = UIImage(named: "fog_night")
+            } else {
+                self.icon.image = UIImage(named: "fog")
+            }
+        }
+            // Tornado / Squalls
+        else if (condition < 800) {
+            self.icon.image = UIImage(named: "tstorm3")
+        }
+            // Sky is clear
+        else if (condition == 800) {
+            if (nightTime){
+                self.icon.image = UIImage(named: "sunny_night")
+            }
+            else {
+                self.icon.image = UIImage(named: "sunny")
+            }
+        }
+            // few / scattered / broken clouds
+        else if (condition < 804) {
+            if (nightTime){
+                self.icon.image = UIImage(named: "cloudy2_night")
+            }
+            else{
+                self.icon.image = UIImage(named: "cloudy2")
+            }
+        }
+            // overcast clouds
+        else if (condition == 804) {
+            self.icon.image = UIImage(named: "overcast")
+        }
+            // Extreme
+        else if ((condition >= 900 && condition < 903) || (condition > 904 && condition < 1000)) {
+            
+            self.icon.image = UIImage(named: "tstorm3")
+        }
+            // Cold
+        else if (condition == 903) {
+            self.icon.image = UIImage(named: "snow5")
+        }
+            // Hot
+        else if (condition == 904) {
+            self.icon.image = UIImage(named: "sunny")
+        }
+            // Weather condition is not available
+        else {
+            self.icon.image = UIImage(named: "dunno")
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("locationManager didFailWithError")
         print(error)
     }
-
+    
 }
 
